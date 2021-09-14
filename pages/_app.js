ae7887452acab8a73720c7269080598427f49afc
Thang <phidinhthang@gmail.com>
2021-09-14 14:22:41 +0700

    final

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
-
+import '../styles/globals.css';
+import NProgressContainer from './lib/component';
+import Link from 'next/link';
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Link href='/'>Home</Link>
+      <Link href='/about'>About</Link>
+      <Link href='/section'>Section</Link>
+      <Link href='/info'>Info</Link>
+
+      <Component {...pageProps} />
+      <NProgressContainer />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/lib/hoc.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import NProgress from './component';
+
+export default (delayMs, options) => (Page) =>
+  class extends Component {
+    static getInitialProps = Page.getInitialProps;
+    render() {
+      return (
+        <>
+          <Page {...this.props} />
+          <NProgress delayMs={delayMs} options={options} />
+        </>
+      );
+    }
+  };
